2a7787db539e65d65c925c56b6160cb362133fe4
Bertz047 <87860203+Bertz047@users.noreply.github.com>
2021-07-23 17:52:43 +0300

    Add files via upload

---
+++ starfford.c
@@ -0,0 +1,60 @@
+/*-----------------------------
+* Version: C99                *
+* Project: Library management *
+* Author: starfford aboki    *
+* Date:    June,2021          *
+* License: MIT                *
+******************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+//Function Prototype
+void execute_action(int action);
+void close();
+int main()
+{
+    int action;
+    printf("\tCounty Library Management System!\n");
+    printf("Welcome aboki\n");
+    // Navigation menu
+    do {
+        printf("1. Add user\n");
+        printf("2. Add Book\n");
+        printf("3. Exit\n");
+        printf("Select action(1-3): ");
+        scanf("%d",&action);
+        // validate input
+        if (action < 1 || action > 3)
+            printf("Invalid action. Try again\n");
+            sleep(2000);
+            system("cls");
+    } while(action < 1 || action > 3);
+
+    return 0;
+}
+void execute_action(int action) {
+    switch(action) {
+    case 1:
+        printf("Adding a user\n");
+        break;
+    case 2:
+        printf("Adding a user\n");
+        break;
+    case 3:
+        printf("Exiting\n");
+        break;
+    default:
+        printf("unknown action.\n");
+
+    }
+}
+void close() {
+    printf("Thank you for using the system\n");
+    printf("Bye\n");
+    Sleep(5000);
+    exit(0);
+
+}
